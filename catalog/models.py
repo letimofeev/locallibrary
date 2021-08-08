416c8d1b8a3df89b675971c1c6af80fd27fbe787
Leonid <63495449+letimofeev@users.noreply.github.com>
2021-08-08 15:48:38 +0300

    author list & author detail view added

--- catalog/models.py
+++ catalog/models.py
@@ -125,6 +125,9 @@ class Author(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
 
+    class Meta:
+        ordering = ['last_name']
+
     def get_absolute_url(self):
         """
         Returns the url to access a particular author instance.
